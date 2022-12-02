c566b54e98c1b4e3512bf3dd0e87019b395302c9
jordan <aminul9365@gmail.com>
2022-12-02 19:22:02 +0530

    initial commit

---
+++ rest-countries-api-with-color-theme-switcher-master/README.md
@@ -0,0 +1,14 @@
+
+
+ users should be able to:
+
+- See all countries from the API on the homepage
+- Search for a country using an `input` field
+- Filter countries by region
+- Click on a country to see more detailed information on a separate page
+- Click through to the border countries on the detail page
+- Toggle the color scheme between light and dark mode;
+
+"First page screen shot :  "restcountries.png"  " 
+"Second page screen shot : "secondpage.png"     "
+
---
+++ rest-countries-api-with-color-theme-switcher-master/index.js
@@ -0,0 +1,201 @@
+let mode = document.getElementById("mode");
+let modetype = document.getElementById("modetype");
+let modeicon = document.getElementById("modeicon");
+let rootvariable = document.querySelector(':root');
+let mapcontainer = Array.from(document.getElementsByClassName("container"));
+let firstpage = document.getElementsByClassName("firstpage")[0];
+let countrydetail = document.getElementsByClassName("countrydetail")[0];
+let back = document.getElementById("back");
+let search = document.getElementById("input");
+let countryflag = document.getElementById("countryflag");
+let population = document.getElementById("population");
+let capital = document.getElementById("capital");
+let selectregion = document.getElementById("Region");
+let secondpage = document.getElementsByClassName("countrydetail")[0];
+
+let cidx = 0;
+back.addEventListener('click', () => {
+
+    firstpage.style.visibility = "visible";
+    secondpage.style.visibility = "hidden"
+
+})
+
+
+
+
+// hsl(200, 15%, 8%)
+let bool = true;
+mode.addEventListener('click', () => {
+
+    if (bool) {
+        rootvariable.style.setProperty('--blue', "hsl(0, 0%, 100%)");
+        rootvariable.style.setProperty('--darkblue', "hsl(240, 3%, 94%)");
+        rootvariable.style.setProperty('--whiite', "hsl(200, 15%, 8%)");
+        rootvariable.style.setProperty('--span', "hsla(298, 0%, 17%, 1) ");
+        modetype.innerHTML = "Light Mode"
+        modeicon.innerHTML = `<i class="fa-solid fa-sun " style = "color:yellow"></i>`
+        bool = false;
+    } else {
+        rootvariable.style.setProperty('--blue', "hsl(209, 23%, 22%)");
+        rootvariable.style.setProperty('--darkblue', "hsl(207, 26%, 17%)");
+        rootvariable.style.setProperty('--whiite', "hsl(0, 0%, 100%)");
+        rootvariable.style.setProperty('--span', " hsl(0%, 13%, 74%)");
+        modetype.innerHTML = "Dark Mode"
+        modeicon.innerHTML = `<i class="fa-solid fa-moon "></i>`
+        bool = true;
+    }
+
+
+})
+
+
+fetch(`https://restcountries.com/v3.1/all`).then(res => res.json()).then(result => {
+    console.log(result)
+
+    result.forEach(element => {
+        createcountry(element)
+
+
+
+    })
+
+
+
+})
+
+
+
+function createcountry(element) {
+
+    let div = document.createElement("div");
+    div.classList.add("container")
+    div.innerHTML = `<img src= ${element.flags["png"]} id="countryflag">
+            <h1 id="countryname" class ="name">${element.name["common"]}</h1>
+            
+            <p>population:  <span id="popolation">${element.population}</span></p>
+            
+            
+            <p> region: <span class="regionname">${element.region}</span></p>
+            
+            <p> capital: <span id="capital">${element.capital}</span></p>`;
+
+
+    document.getElementsByClassName("countrycontainer")[0].appendChild(div);
+
+    div.addEventListener('click', (e) => {
+        firstpage.style.display = "none"
+        secondpage.style.visibility = "visible"
+        document.getElementById("capital1").innerHTML = ` ${element.capital}`
+
+
+        showdetail(element);
+
+    })
+
+
+
+
+
+
+
+}
+
+
+
+
+let container = document.getElementsByClassName("container");
+let dropdown = document.getElementById("Region");
+let countryname = document.getElementsByClassName("name");
+let region = document.getElementsByClassName("regionname");
+
+
+dropdown.addEventListener('click', function (e) {
+
+    Array.from(region).forEach(elem => {
+
+        if (elem.innerHTML.toLowerCase().includes(this.value.toLowerCase())) {
+
+            elem.parentElement.parentElement.style.display = "flex"
+
+        } else {
+            elem.parentElement.parentElement.style.display = "none"
+        }
+
+
+
+    })
+
+
+
+})
+
+search.addEventListener('input', () => {
+
+    Array.from(countryname).forEach(elem => {
+
+        if (elem.innerHTML.toLowerCase().includes(search.value.toLowerCase())) {
+
+            elem.parentElement.style.display = "flex"
+
+        } else {
+            elem.parentElement.style.display = "none"
+        }
+
+
+
+    })
+})
+
+
+document.addEventListener('click',(e)=>{
+if(e.keycode === 13 || e.keycode =="Enter"){
+
+   
+   console.log("worked")
+
+
+}
+
+})
+
+
+
+function showdetail(data) {
+    let nativename = Object.keys(data.name["nativeName"]);
+    let currency = Object.keys(data.currencies);
+    let topld = (data.tld);
+    console.log(topld[0])
+    let language = Object.keys(data.languages);
+   
+
+
+    document.getElementById("countryflag2").innerHTML = ` <img src= ${data.flags["png"]} alt="" class="flag">`
+    document.getElementById("countryname2").innerHTML = `${data.name["common"]}`
+    document.getElementById("population2").innerHTML = `${data.population}`
+    document.getElementById("region2").innerHTML = `${data.region}`
+    document.getElementById("sub-region").innerHTML = `${data["subregion"]}`
+    document.getElementById("rupees").innerHTML = `${data.currencies[`${currency}`]["name"]}`
+    document.getElementById("language1").innerHTML = `${data.languages[language[0]]}`
+    document.getElementById("topdomain").innerHTML = `${topld[0]}`
+    document.getElementById("native").innerHTML = `${data.name["nativeName"][`${nativename[0]}`]["official"]}`
+    document.getElementById("border1").innerHTML = `${data.borders[0] == "undefined" ?  '_':data.borders[0]}`
+    document.getElementById("border2").innerHTML = `${data.borders[1]}`
+    document.getElementById("border3").innerHTML = `${data.borders[2]}`
+
+
+
+
+}
+
+
+
+
+
+
+
+back.addEventListener('click', () => {
+    firstpage.style.display = "block"
+    secondpage.style.visibility = "hidden"
+
+})
